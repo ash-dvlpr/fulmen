47cb24aa65b3b1b6d893e3c480a47277bd992958
Λsh Dev <69819379+ash-dvlpr@users.noreply.github.com>
2022-07-04 10:59:02 +0200

    Improved Cargo manifests
    Co-authored-by: Xeros08 <42175313+Xeros08@users.noreply.github.com>

--- lvengine/src/vulkan/utils.rs
+++ lvengine/src/vulkan/utils.rs
@@ -3,7 +3,7 @@ use std::ffi;
 
 // ============ FFI Stuff ============
 /// External callback function used by the validation layer 
-/// TODO : Use a logging crate instead of directly 
+/// TODO : Use a logging crate instead of directly printing to stdout
 #[cfg(feature = "validation_layers")]
 pub(crate) unsafe extern "system" fn vulkan_debug_utils_callback(
     message_severity: vk::DebugUtilsMessageSeverityFlagsEXT, // Severities that trigger the callback
@@ -14,6 +14,7 @@ pub(crate) unsafe extern "system" fn vulkan_debug_utils_callback(
     let message = ffi::CStr::from_ptr((*p_callback_data).p_message);
     let severity = format!("{:?}", message_severity).to_lowercase();
     let ty = format!("{:?}", message_type).to_lowercase();
+    
     println!("[VULKAN][{}][{}] {:?}", severity, ty, message);
     vk::FALSE
 }
