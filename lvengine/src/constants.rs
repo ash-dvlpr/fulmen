b7ccee64da50ff7f9b4f2e7f9f0108f818a4d5bd
Λsh Dev <69819379+ash-dvlpr@users.noreply.github.com>
2022-07-04 14:42:08 +0200

    Switched to Vulkan 1.2
    Co-authored-by: Xeros08 <42175313+Xeros08@users.noreply.github.com>

--- lvengine/src/constants.rs
+++ lvengine/src/constants.rs
@@ -12,7 +12,7 @@ pub(crate) const DEFAULT_APP_VERSION: u32 = vk::make_api_version(0, 0, 0, 1);
 
 pub(crate) const ENGINE_NAME: &str = "Little Vulkan Engine";
 pub(crate) const ENGINE_VERSION: u32 = vk::make_api_version(0, 0, 0, 1);
-pub(crate) const VK_VERSION: u32 = vk::API_VERSION_1_3;
+pub(crate) const VK_VERSION: u32 = vk::API_VERSION_1_2;
 
 
 // ? Alvaliable Layers and Extensions
