9eb5032330c61e5e785ecfb7ae4cb222eac123f2
Ash Dev <69819379+ash-dvlpr@users.noreply.github.com>
2025-03-03 21:11:25 +0100

    [WIP] ECS improve `TypeIdMap` to avoid re-hashing the `TypeId` with a custom Hasher.

---
+++ modules/fulmen_ecs/src/utils/hash.rs
@@ -0,0 +1,35 @@
+use core::hash::{BuildHasher, Hasher};
+
+/// [`BuildHasher`] for types that already contain a high-quality hash.
+#[derive(Clone, Default)]
+pub struct NoOpHash;
+
+impl BuildHasher for NoOpHash {
+    type Hasher = NoOpHasher;
+
+    fn build_hasher(&self) -> Self::Hasher {
+        NoOpHasher::default()
+    }
+}
+
+#[doc(hidden)]
+#[derive(Default)]
+pub struct NoOpHasher(u64);
+
+// This for types that act like hashes, to avoid re-hashing that value.
+impl Hasher for NoOpHasher {
+    fn finish(&self) -> u64 {
+        self.0
+    }
+
+    fn write(&mut self, bytes: &[u8]) {
+        self.0 = bytes.iter().fold(self.0, |hash, b| {
+            hash.rotate_left(8).wrapping_add(*b as u64)
+        });
+    }
+
+    #[inline]
+    fn write_u64(&mut self, i: u64) {
+        self.0 = i;
+    }
+}
--- modules/fulmen_ecs/src/utils/mod.rs
+++ modules/fulmen_ecs/src/utils/mod.rs
@@ -1,2 +1,8 @@
+mod hash;
+
+use hash::NoOpHash;
 use std::{any::TypeId, collections::HashMap};
-pub type TypeIdMap<V> = HashMap<TypeId, V>;
+
+/// HashMap that uses the [`TypeId`] directly as a key.
+/// As [`TypeId`]s are guaranteed to be unique, this HashMap uses a [`NoOpHash`] to skip re-hashing the key.
+pub type TypeIdMap<V> = HashMap<TypeId, V, NoOpHash>;
