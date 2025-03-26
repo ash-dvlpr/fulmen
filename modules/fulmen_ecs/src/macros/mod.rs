187dcfbb58c695ac4dfbdf0365a28c825a89b19e
Ash Dev <69819379+ash-dvlpr@users.noreply.github.com>
2025-03-26 23:53:22 +0100

    [WIP] Utility Macro for generating Tuple implementations

--- modules/fulmen_ecs/src/macros/mod.rs
+++ modules/fulmen_ecs/src/macros/mod.rs
@@ -22,3 +22,32 @@ macro_rules! impl_component {
         }
     };
 }
+
+/// Reverses the parameters passed to the before calling m!()
+#[macro_export]
+macro_rules! reverse_and_call {
+    // Base case with no [pending] elements, calls `m`.
+    ($m:ident [] $($reversed:tt)*) => {
+        $m!{$($reversed),*}
+    };
+    ($m:ident [$first:tt $($rest:tt)*] $($reversed:tt)*) => {
+        reverse_and_call!{$m [$($rest)*] $first $($reversed)*}
+    };
+}
+
+// Generates variadic macro calls for
+//
+// Calls m!() with up to the specified number of params
+// to generate code for fake variadic implementations.
+#[macro_export]
+macro_rules! gen_variadic_macro_calls {
+    // Base case for the recursive macro
+    ($m: ident, $param: tt) => {
+        $m!{$param}
+    };
+    // 
+    ($m: ident, $param: tt, $($rest_params: tt),*) => {
+        gen_variadic_macro_calls!{$m, $($rest_params),*}
+        reverse_and_call!{$m [$param $($rest_params)*]}
+    };
+}
--- modules/fulmen_ecs/tests/macros_should_compile.rs
+++ modules/fulmen_ecs/tests/macros_should_compile.rs
@@ -11,3 +11,15 @@ impl_component!(TestCompB);
 #[allow(dead_code)]
 struct TestRes(pub u32);
 impl_resource!(TestRes);
+
+
+pub trait TestTrait { }
+macro_rules! impl_test_trait {
+    () => {};
+    ($($name:ident),*) => {
+        impl<$($name),*> TestTrait for ($($name,)*) { }
+    };
+}
+
+// Generate tuples up to 8 elements (expand as needed)
+gen_variadic_macro_calls!(impl_test_trait, D, C, B, A);
